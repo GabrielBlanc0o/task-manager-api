b7e110c141ac784856df61defa532bc1de8f30a2
GabrielBlanc0o <gtbvacation@outlook.es>
2026-07-01 10:51:21 -0500

    feat: enhance comments for clarity in TaskmanagerApplication

--- src/main/java/com/gabrielblanco/taskmanager/TaskmanagerApplication.java
+++ src/main/java/com/gabrielblanco/taskmanager/TaskmanagerApplication.java
@@ -3,11 +3,13 @@ package com.gabrielblanco.taskmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// use este @ para inicial punto de entrada a una app SpringBoot
 @SpringBootApplication
 public class TaskmanagerApplication {
 
 	public static void main(String[] args) {
+		// con este levantamos el servidor en el puente 8080
 		SpringApplication.run(TaskmanagerApplication.class, args);
 	}
-
 }
